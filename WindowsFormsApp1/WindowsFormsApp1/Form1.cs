c6a7a7f81931e462a754fe8201e6daf6576901ff
Werefi <1@303-18>
2020-02-20 17:37:48 +0500

    4

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,59 +12,67 @@ using System.Data.SqlClient;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=303-18; Initial Catalog=Login; Integrated Security=true;");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
-            SqlConnection con = new SqlConnection("Data Source=303-18; Initial Catalog=Login; Integrated Security=true;");
-
-            public Form1()
-                {
-                    InitializeComponent();
-                }
-
-            private void button1_Click(object sender, EventArgs e)
+            con.Open();
+            string s = "SELECT Users FROM dbo.login WHERE Users = '" + textBox1.Text + "' AND password = '" + textBox2.Text + "' ";
+            SqlCommand com = new SqlCommand(s, con);
+            MessageBox.Show(s);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
             {
+                MessageBox.Show("GOod");
+                Form2 fm = new Form2();
+                fm.Show();
+                this.Hide();
+            }
+            else
+            {
+                if (Properties.Settings.Default.time <= DateTime.UtcNow) ;
+                {
+                    Properties.Settings.Default.attempts++;
+                    if (Properties.Settings.Default.attempts == 3) ;
+                    {
+                        Properties.Settings.Default.time = DateTime.UtcNow.AddMinutes(1);
+                        Properties.Settings.Default.attempts = 0;
+                        button1.Enabled = false;
 
-                    con.Open();
-                    SqlCommand com = new SqlCommand($"SELECT Users FROM login where Users = '"+textBox1.Text+"' and password = '"+textBox2.Text+"' ", con);
-                    SqlDataReader dr = com.ExecuteReader();
-                    if (dr.HasRows)
-                        {
-                            MessageBox.Show("GOod");
-                            Form2 fm = new Form2();
-                            fm.Show();
-                            this.Hide();
-                        }
-                    else
-                        {
-                            if (Properties.Settings.Default.time <= DateTime.UtcNow) ;
-                            {
-                                Properties.Settings.Default.attempts++;
-                                if (Properties.Settings.Default.attempts == 3) ;
-                                {
-                                    Properties.Settings.Default.time = DateTime.UtcNow.AddMinutes(1);
-                                    Properties.Settings.Default.attempts = 0;
-                                    button1.Enabled = false;
-
-                                }
+                    }
 
 
-                            }
-                            MessageBox.Show("BAD");
+                }
+                MessageBox.Show("BAD");
 
-                        }
-                    con.Close();
+            }
+                con.Close();
             
-            }
+        }
 
-            private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.time <= DateTime.UtcNow)
             {
-                if (Properties.Settings.Default.time <= DateTime.UtcNow)
-                {
-                    button1.Enabled = true;
-                }
+                button1.Enabled = true;
             }
-        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
+
+
 
 
 }
