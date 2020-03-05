6ea7d6101ad2f8040859cf00674aab00886ec3cd
Werefi <1@303-18>
2020-03-05 16:31:40 +0500

    141212414

--- WindowsFormsApp1/WindowsFormsApp1/Form4.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form4.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 fm = new Form3();
+            fm.Show();
+            this.Hide();
+        }
     }
 }
