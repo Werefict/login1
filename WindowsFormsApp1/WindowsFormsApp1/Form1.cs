288cb6fccdc23e11e0502ac0659cd49d1135a30b
Werefi <1@303-18>
2020-03-05 16:29:41 +0500

    12341

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
-        SqlConnection con = new SqlConnection("Data Source=303-18; Initial Catalog=Login; Integrated Security=true;");
+        SqlConnection con = new SqlConnection("Data Source=303-18; Initial Catalog=Money; Integrated Security=true;");
 
         public Form1()
         {
@@ -23,36 +23,38 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             con.Open();
-            SqlCommand com = new SqlCommand("SELECT Users FROM dbo.login WHERE Users = '" + textBox1.Text + "' ", con);// AND password = '" + textBox2.Text + "' ", con);
+            SqlCommand com = new SqlCommand($"SELECT Login_executor FROM [dbo].[Executer] WHERE Login_executor = '{textBox1.Text}' ", con);// AND password = '" + textBox2.Text + "' ", con);
             SqlDataReader dr = com.ExecuteReader();
+            Console.WriteLine(dr.HasRows);
             if (dr.HasRows)
             {
-                MessageBox.Show("GOod");
-                Form2 fm = new Form2();
+               // MessageBox.Show("GOod");
+                Form4 fm = new Form4();
                 fm.Show();
                 this.Hide();
             }
-            else
-            {
-                if (Properties.Settings.Default.time <= DateTime.UtcNow) ;
-                {
-                    Properties.Settings.Default.attempts++;
-                    if (Properties.Settings.Default.attempts == 3) ;
-                    {
-                        Properties.Settings.Default.time = DateTime.UtcNow.AddMinutes(1);
-                        Properties.Settings.Default.attempts = 0;
-                        button1.Enabled = false;
+            //else
+            //{
+            //    if (Properties.Settings.Default.time <= DateTime.UtcNow) ;
+            //    {
+            //        Properties.Settings.Default.attempts++;
+            //        if (Properties.Settings.Default.attempts == 3) ;
+            //        {
+            //            Properties.Settings.Default.time = DateTime.UtcNow.AddMinutes(1);
+            //            Properties.Settings.Default.attempts = 0;
+            //            button1.Enabled = false;
 
-                    }
+            //        }
 
 
-                }
-                MessageBox.Show("BAD");
+            //    }
+            //    MessageBox.Show("Повторите попытку");
 
-            }
-                con.Close();
+            //}
+            dr.Close();
+            Console.WriteLine(com.ExecuteScalar());
+            con.Close();
             
         }
 
@@ -65,6 +67,11 @@ namespace WindowsFormsApp1
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
